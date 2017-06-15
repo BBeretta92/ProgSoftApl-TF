a4cca9419d5a2229ac84131a9694bc07898c33e1
BBeretta92 <brunopberetta@gmail.com>
2017-06-15 16:29:33 -0300

    AppStart updated!

--- PSA-TF-Final/PSA-TF-Final/App_Start/BundleConfig.cs
+++ PSA-TF-Final/PSA-TF-Final/App_Start/BundleConfig.cs
@@ -5,7 +5,7 @@ namespace PSA_TF_Final
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
@@ -14,8 +14,8 @@ namespace PSA_TF_Final
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
@@ -25,7 +25,26 @@ namespace PSA_TF_Final
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datepicker.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+                      "~/Scripts/script.js",
+                      "~/Scripts/jquery-ui-1.12.1.js",
+                      "~/Scripts/jquery-ui-1.12.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/locales/bootstrap-datepicker.pt-BR.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/inputmask").Include(
+            "~/Scripts/jquery.inputmask/inputmask.js",
+            "~/Scripts/jquery.inputmask/jquery.inputmask.js",
+            "~/Scripts/jquery.inputmask/inputmask.extensions.js",
+            "~/Scripts/jquery.inputmask/inputmask.date.extensions.js",
+            //and other extensions you want to include
+            "~/Scripts/jquery.inputmask/inputmask.numeric.extensions.js"));
+
         }
     }
 }
